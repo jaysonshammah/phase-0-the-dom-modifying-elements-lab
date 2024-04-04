b561a3204d25b3ff79b6f080ef109255976f21d8
jayson <jaysonshammah3@gmail.com>
2024-04-04 09:18:53 +0300

    add dom

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const yourName = 'John Doe'; 
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
